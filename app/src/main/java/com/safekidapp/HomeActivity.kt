997c8f17af5b16675b2848c17db80e432a7b1c65
Antonio Herazo <antonioherazo7@hotmail.com>
2026-07-14 23:53:54 -0500

    test: plain Activity without theme

--- app/src/main/java/com/safekidapp/HomeActivity.kt
+++ app/src/main/java/com/safekidapp/HomeActivity.kt
@@ -1,10 +1,10 @@
 package com.safekidapp
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val tv = TextView(this)
