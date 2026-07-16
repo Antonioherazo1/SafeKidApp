bde705522992c03a594257849680e6b078d99004
Antonio Herazo <antonioherazo7@hotmail.com>
2026-07-16 00:20:24 -0500

    fix: add TextInputEditText import to LoginActivity

--- app/src/main/java/com/safekidapp/LoginActivity.kt
+++ app/src/main/java/com/safekidapp/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
 
